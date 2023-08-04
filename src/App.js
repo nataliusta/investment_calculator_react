bc9e0ed0e30c90d0164948d6f548cd0db7388f03
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2023-08-04 13:33:12 +0300

    Create CalculatorTable component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './assets/investment-calculator-logo.png';
 import CalculatorForm from './components/CalculatorForm';
+import CalculatorTable from './components/CalculatorTable';
 
 function App() {
   const calculateHandler = (userInput) => {
@@ -37,30 +38,10 @@ function App() {
         <h1>Investment Calculator</h1>
       </header>
       <CalculatorForm />
-
       {/* Todo: Show below table conditionally (only once result data is available) */}
       {/* Show fallback text if no data is available */}
 
-      <table className="result">
-        <thead>
-          <tr>
-            <th>Year</th>
-            <th>Total Savings</th>
-            <th>Interest (Year)</th>
-            <th>Total Interest</th>
-            <th>Invested Capital</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>YEAR NUMBER</td>
-            <td>TOTAL SAVINGS END OF YEAR</td>
-            <td>INTEREST GAINED IN YEAR</td>
-            <td>TOTAL INTEREST GAINED</td>
-            <td>TOTAL INVESTED CAPITAL</td>
-          </tr>
-        </tbody>
-      </table>
+      <CalculatorTable />
     </div>
   );
 }
