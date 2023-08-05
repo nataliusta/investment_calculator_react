804741240047e3d8f1d5abf969392a542191e172
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2023-08-05 14:51:19 +0300

    Add formatter to CalculatorTable to format values

--- src/components/CalculatorTable.js
+++ src/components/CalculatorTable.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import styles from './CalculatorTable.module.css';
 
+const formatter = new Intl.NumberFormat('en-Us', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+});
+
 const CalculatorTable = (props) => {
     return (
         <table className={styles.result}>
@@ -15,16 +22,20 @@ const CalculatorTable = (props) => {
             </thead>
             <tbody>
                 {props.data.map((yearData) => (
-                    <tr>
+                    <tr key={yearData.year}>
                         <td>{yearData.year}</td>
-                        <td>{yearData.savingsEndOfYear}</td>
-                        <td>{yearData.yearlyInterest}</td>
-                        <td>{yearData.savingsEndOfYear - 
+                        <td>{formatter.format
+                            (yearData.savingsEndOfYear)}</td>
+                        <td>{formatter.format
+                            (yearData.yearlyInterest)}</td>
+                        <td>{formatter.format
+                            (yearData.savingsEndOfYear - 
                              props.initialInvestment - 
                              yearData.yearlyContribution * 
-                             yearData.year}</td>
-                        <td>{props.initialInvestment + 
-                             yearData.yearlyContribution * yearData.year}</td>
+                             yearData.year)}</td>
+                        <td>{formatter.format
+                            (props.initialInvestment + 
+                             yearData.yearlyContribution * yearData.year)}</td>
                     </tr>
                 ))}
             </tbody>
